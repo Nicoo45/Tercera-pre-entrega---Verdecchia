4c9fa99873c4a755038b8546a6317f4e56b26f55
Nicoo45 <NICOLAS.VERDECCHIA@HOTMAIL.COM>
2023-02-28 21:01:11 -0300

    Clubs V1 Herencia de Templates y Nav. de estos OK

--- allclubs/urls.py
+++ allclubs/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from allclubs import views
 
 urlpatterns = [
-    path('', views.Inicio),
+    path('', views.Inicio, name="Inicio"),
     path('Clubs', views.Clubs, name="Clubs"),
-    path('Entrenadores', views.Entrenadores),
-    path('Deportistas', views.Deportistas),
-    path('Equipamiento', views.Equipamiento),    
+    path('Entrenadores', views.Entrenadores, name="Entrenadores"),
+    path('Deportistas', views.Deportistas, name="Deportistas"),
+    path('Equipamiento', views.Equipamiento, name="Equipamiento"),    
 ]
